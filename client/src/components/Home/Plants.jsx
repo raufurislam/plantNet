d8b99671426265f9587870c16f575854335e8da1
raufurislam <raufurislam@gmail.com>
2025-03-03 13:26:58 +0600

    Image upload with imgbb

--- client/src/components/Home/Plants.jsx
+++ client/src/components/Home/Plants.jsx
@@ -1,14 +1,21 @@
-import Card from './Card'
-import Container from '../Shared/Container'
+import Card from "./Card";
+import Container from "../Shared/Container";
 
 const Plants = () => {
   return (
     <Container>
-      <div className='pt-12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8'>
+      <div className="pt-12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-5">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
         <Card />
       </div>
     </Container>
-  )
-}
+  );
+};
 
-export default Plants
+export default Plants;
